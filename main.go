f9cb577873d3cd319d563946251accf34955c514
Atila Romero <atilaromero@gmail.com>
2020-09-08 16:01:08 -0300

    add command line options

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"os"
 )
@@ -8,6 +9,11 @@ import (
 //go:generate go run generate/main.go
 
 func main() {
+	shouldListen := flag.Bool("listen", false, "if the server should receive jobs via API")
+	shouldWatch := flag.Bool("watch", false, "if the server should use polling to get jobs")
+	path := flag.String("path", "", "path to a datasource, to create a single job")
+	flag.Parse()
+
 	jar := assertEnv("IPEDJAR")
 	locker := remoteLocker{
 		URL: assertEnv("LOCK_URL"),
@@ -17,15 +23,38 @@ func main() {
 	if !ok {
 		PORT = "80"
 	}
-	watchURL, isWatching := os.LookupEnv("WATCH_URL")
+
+	watchURL := ""
+	if *shouldWatch {
+		watchURL = assertEnv("WATCH_URL")
+	}
+
+	hasJob := (*path != "")
+
+	count := 0
+	for _, x := range []bool{*shouldListen, *shouldWatch, hasJob} {
+		if x {
+			count = count + 1
+		}
+	}
+	if count == 0 {
+		log.Fatalf("One of --listen --watch or --path should be used")
+	}
+	if count > 1 {
+		log.Fatalf("Only one of --listen --watch or --path should be used")
+	}
 
 	Serve(ServeOptions{
-		locker:      &locker,
-		PORT:        PORT,
-		watchURL:    watchURL,
-		isWatching:  isWatching,
-		jar:         jar,
-		notifierURL: notifierURL,
+		locker:       &locker,
+		PORT:         PORT,
+		watchURL:     watchURL,
+		shouldWatch:  *shouldWatch,
+		jar:          jar,
+		notifierURL:  notifierURL,
+		hasJob:       hasJob,
+		shouldListen: *shouldListen,
+	}, Job{
+		EvidencePath: *path,
 	})
 }
 
--- server.go
+++ server.go
@@ -15,145 +15,96 @@ import (
 
 // ServeOptions has options for Serve()
 type ServeOptions struct {
-	locker      *remoteLocker
-	PORT        string
-	watchURL    string
-	isWatching  bool
-	jar         string
-	notifierURL string
+	locker       *remoteLocker
+	PORT         string
+	notifierURL  string
+	jar          string
+	shouldWatch  bool
+	watchURL     string
+	shouldListen bool
+	hasJob       bool
 }
 
 // Serve creates the web server
-func Serve(options ServeOptions) {
-	if options.isWatching {
-		router := WatchRouter(options.locker)
-		go http.ListenAndServe(fmt.Sprintf(":%s", options.PORT), router)
-
-		ctxWatch, ctxCancel := context.WithTimeout(context.Background(), 1*time.Hour)
-		defer ctxCancel()
-		watch(ctxWatch, options.watchURL, options.jar, options.locker, options.notifierURL)
-	} else {
-		router := LazyRouter(options.locker, options.notifierURL, options.jar)
-		log.Println("Listening on port", options.PORT)
-		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", options.PORT), router))
-	}
-}
-
-// WatchRouter returns a router that actively looks for jobs
-func WatchRouter(locker *remoteLocker) *mux.Router {
+func Serve(options ServeOptions, job Job) {
+	metrics := createIpedMetrics()
 	router := mux.NewRouter()
+	endpoints := []string{}
 
-	router.HandleFunc("/healthz",
-		func(w http.ResponseWriter, r *http.Request) {
-			defer r.Body.Close()
-			w.Write([]byte("ok"))
-		}).Methods("GET")
+	router.HandleFunc("/healthz", healthz).Methods("GET")
+	endpoints = append(endpoints, "/healthz")
 
-	router.HandleFunc("/readiness",
-		func(w http.ResponseWriter, r *http.Request) {
-			defer r.Body.Close()
-			if locker.EvidencePath != "" {
-				http.Error(w, "not ready", http.StatusServiceUnavailable)
-				return
-			}
-			w.Write([]byte("ok"))
-		}).Methods("GET")
+	router.HandleFunc("/readiness", readiness(options)).Methods("GET")
+	endpoints = append(endpoints, "/readiness")
 
 	router.Handle("/metrics", promhttp.Handler())
+	endpoints = append(endpoints, "/metrics")
 
-	return router
+	if options.shouldListen {
+		router.HandleFunc("/start",
+			func(w http.ResponseWriter, r *http.Request) {
+				defer r.Body.Close()
+				w.Header().Set("Access-Control-Allow-Origin", "*")
+				w.Header().Set("Access-Control-Allow-Methods", "POST")
+				w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+			}).Methods("OPTIONS")
+
+		router.HandleFunc("/start",
+			listenForJobs(options.jar, options.locker, options.notifierURL, metrics)).Methods("POST")
+		endpoints = append(endpoints, "/start")
+	}
+
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	srv := &http.Server{Addr: fmt.Sprintf(":%s", options.PORT), Handler: router}
+	srv.Shutdown(ctx)
+	go func() {
+		log.Println("Listening on port", options.PORT)
+		log.Println("Endpoints:")
+		for _, x := range endpoints {
+			log.Println(x)
+		}
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+			// unexpected error. port in use?
+			log.Fatalf("ListenAndServe(): %v", err)
+		}
+	}()
+
+	if options.hasJob {
+		log.Printf("using single job: %s\n", job.EvidencePath)
+		processPayloads(ctx, []Job{job}, options.jar, options.locker, options.notifierURL, metrics)
+		cancel()
+	}
+
+	if options.shouldWatch {
+		log.Printf("watching URL: %s\n", options.watchURL)
+		go watch(ctx, options.watchURL, options.jar, options.locker, options.notifierURL, metrics)
+	}
+	<-ctx.Done()
 }
 
-// LazyRouter returns a router that receives jobs via web API. It uses WatchRouter
-func LazyRouter(locker *remoteLocker, notifierURL string, jar string) *mux.Router {
-	router := WatchRouter(locker)
-
-	router.HandleFunc("/start",
-		listen(jar, locker, notifierURL)).Methods("POST")
-
-	router.HandleFunc("/start",
-		func(w http.ResponseWriter, r *http.Request) {
-			defer r.Body.Close()
-			w.Header().Set("Access-Control-Allow-Origin", "*")
-			w.Header().Set("Access-Control-Allow-Methods", "POST")
-			w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-		}).Methods("OPTIONS")
-
-	router.HandleFunc("/swagger.json",
-		func(w http.ResponseWriter, r *http.Request) {
-			defer r.Body.Close()
-			w.Header().Set("Access-Control-Allow-Origin", "*")
-			w.Write([]byte(generatedSwagger))
-		})
-	return router
+func healthz(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	w.Write([]byte("ok"))
 }
 
-func watch(ctx context.Context, watchURL, jar string, locker *remoteLocker, notifierURL string) {
-	metrics := createIpedMetrics()
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		default:
-			r, err := http.Get(watchURL)
-			if err != nil {
-				log.Fatalf("could not watch URL: %v\n", err)
-			}
-			defer r.Body.Close()
-			var payloads []todo
-			b, err := ioutil.ReadAll(r.Body)
-			if err != nil {
-				log.Fatalf("could not read watch body: %v\n", err)
-			}
-			err = json.Unmarshal(b, &payloads)
-			if err != nil {
-				log.Fatalf("could not parse JSON: %v; data: %v\n", err, string(b))
-			}
-			if len(payloads) < 1 {
-				time.Sleep(5 * time.Second)
-				continue
-			}
-			ctxPayloads, cancelPayload := context.WithTimeout(context.Background(), 60*time.Second)
-			defer cancelPayload()
-			processPayloads(ctxPayloads, payloads, jar, locker, notifierURL, metrics)
+func readiness(options ServeOptions) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		if options.locker.EvidencePath != "" {
+			http.Error(w, "not ready", http.StatusServiceUnavailable)
 			return
 		}
+		w.Write([]byte("ok"))
 	}
 }
 
-func processPayloads(ctx context.Context, payloads []todo, jar string, locker *remoteLocker, notifierURL string, metrics ipedMetrics) {
-	var err error
-	for _, payload := range payloads {
-		select {
-		case <-ctx.Done():
-			if err != nil {
-				log.Fatalf("error: %v\n", err)
-			}
-			return
-		default:
-			params := ipedParams{
-				jar:             jar,
-				evidence:        payload.EvidencePath,
-				output:          payload.OutputPath,
-				profile:         payload.Profile,
-				additionalArgs:  payload.AdditionalArgs,
-				additionalPaths: payload.AdditionalPaths,
-			}
-			err = runIped(params, locker, notifierURL, metrics)
-			if err != nil {
-				fmt.Printf("error: %v\n", err)
-			}
-		}
-	}
-}
-
-func listen(jar string, locker *remoteLocker, notifierURL string) func(w http.ResponseWriter, r *http.Request) {
-	metrics := createIpedMetrics()
-
+func listenForJobs(jar string, locker *remoteLocker, notifierURL string, metrics ipedMetrics) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		defer r.Body.Close()
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		var payload todo
+		var payload Job
 		err := json.NewDecoder(r.Body).Decode(&payload)
 		if err != nil {
 			http.Error(w, fmt.Sprintf("could not parse JSON: %s", err.Error()), http.StatusBadRequest)
@@ -184,7 +135,65 @@ func listen(jar string, locker *remoteLocker, notifierURL string) func(w http.Re
 	}
 }
 
-type todo struct {
+func watch(ctx context.Context, watchURL, jar string, locker *remoteLocker, notifierURL string, metrics ipedMetrics) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			r, err := http.Get(watchURL)
+			if err != nil {
+				log.Fatalf("could not watch URL: %v\n", err)
+			}
+			defer r.Body.Close()
+			var payloads []Job
+			b, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				log.Fatalf("could not read watch body: %v\n", err)
+			}
+			err = json.Unmarshal(b, &payloads)
+			if err != nil {
+				log.Fatalf("could not parse JSON: %v; data: %v\n", err, string(b))
+			}
+			if len(payloads) < 1 {
+				time.Sleep(5 * time.Second)
+				continue
+			}
+			ctxPayloads, cancelPayload := context.WithTimeout(context.Background(), 60*time.Second)
+			defer cancelPayload()
+			processPayloads(ctxPayloads, payloads, jar, locker, notifierURL, metrics)
+			return
+		}
+	}
+}
+
+func processPayloads(ctx context.Context, payloads []Job, jar string, locker *remoteLocker, notifierURL string, metrics ipedMetrics) {
+	var err error
+	for _, payload := range payloads {
+		select {
+		case <-ctx.Done():
+			if err != nil {
+				log.Fatalf("error: %v\n", err)
+			}
+			return
+		default:
+			params := ipedParams{
+				jar:             jar,
+				evidence:        payload.EvidencePath,
+				output:          payload.OutputPath,
+				profile:         payload.Profile,
+				additionalArgs:  payload.AdditionalArgs,
+				additionalPaths: payload.AdditionalPaths,
+			}
+			err = runIped(params, locker, notifierURL, metrics)
+			if err != nil {
+				fmt.Printf("error: %v\n", err)
+			}
+		}
+	}
+}
+
+type Job struct {
 	EvidencePath    string `json:"evidencePath,omitempty"`
 	OutputPath      string `json:"outputPath,omitempty"`
 	Profile         string `json:"profile,omitempty"`
